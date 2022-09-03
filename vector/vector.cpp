6b223ddfd3d1f43f9c22aa4d9a8128a935b82741
kelinci dev <61102653+kelincidev@users.noreply.github.com>
2022-09-03 10:54:57 +1000

    Create vector.cpp

---
+++ vector/vector.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+main() {
+    vector<int> vec2D;
+    vec2D.push_back(10);
+    vec2D.push_back(40);
+
+    cout<<"vector a "<<vec2D[0]<<" "<<vec2D[1]<<endl;
+}
